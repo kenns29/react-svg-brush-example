6d2704efa5de2684241fc1d12f85a968c5ad7554
Hong Wang <hongw@uber.com>
2019-09-03 09:40:12 -0700

    remove aliases

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,29 @@
-const {resolve} = require('path');
-const webpack = require('webpack');
+const { resolve } = require("path");
+const webpack = require("webpack");
 
 module.exports = {
   entry: {
-    app: resolve('./app.js')
+    app: resolve("./app.js")
   },
-  devtool: 'source-maps',
-  resolve: {
-    alias: {
-      'react-svg-brush': resolve('../react-svg-brush')
-    }
-  },
+  devtool: "source-maps",
   module: {
     rules: [
       {
         test: /\.js$/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         exclude: [/node_modules/],
-        include: [resolve('.')]
+        include: [resolve(".")]
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ["style-loader", "css-loader"]
       },
       {
         test: /\.(png)$/,
         use: [
           {
-            loader: 'file-loader',
-            options: {name: '[path][name].[ext]'}
+            loader: "file-loader",
+            options: { name: "[path][name].[ext]" }
           }
         ]
       }
